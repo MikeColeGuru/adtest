fd8e7b5ed16d9387483c9679dabe4a8b83f3efdc
Mike Cole <mike@dragonsguild.net>
2018-09-01 06:10:08 -0500

    user2

--- adtest.py
+++ adtest.py
@@ -1,7 +1,7 @@
 from ldap3 import Server, Connection, ALL, NTLM
 
-server = Server('prdc1b.dms.local',  get_info=ALL)
+server = Server('ldaps://prdc1b.dms.local',  get_info=ALL)
 conn = Connection(server, user="DMS\\user", password="", authentication=NTLM)
-# conn.bind()
+conn.bind()
 # print(server.info)
-conn.extend.standard.who_am_i()
+print(conn.extend.standard.who_am_i())
