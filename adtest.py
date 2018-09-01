bf81346395500c3d11ee66e26e2235f8ee422bed
Mike Cole <mike@dragonsguild.net>
2018-09-01 05:58:43 -0500

    server info

--- adtest.py
+++ adtest.py
@@ -1,5 +1,5 @@
 from ldap3 import Server, Connection, ALL
 
-server = Server('prdc1b.dms.local')
+server = Server('prdc1b.dms.local',  get_info=ALL)
 conn = Connection(server)
-print(conn.bind())
+print(server.info)
